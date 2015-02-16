e95e5597383cd9200a6b3aae983f0b9de2f17f03
Marcin Wyszynski <marcin.pixie@gmail.com>
2015-02-16 10:43:37 +0000

    each paper needs its own subpage

--- app/controllers/papers_controller.rb
+++ app/controllers/papers_controller.rb
@@ -2,4 +2,9 @@ class PapersController < ApplicationController
   def index
     @papers = Paper::where('archived_at IS NULL').to_a.shuffle!
   end
+
+  def show
+    @paper = Paper::find(params[:id])
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   root to: 'papers#index'
+  resources :papers, only: [:show]
 end
